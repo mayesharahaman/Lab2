8b4868b710b13380b868d79b1681beb862b76f0b
Mayesha Rahaman <mayesha.rahaman@mail.utoronto.ca>
2024-09-16 15:34:16 -0400

    task_1 completed

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,8 +3,8 @@ import java.util.List;
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
-
-        int s = 0;
+        //int s = 0 doesn't work
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
